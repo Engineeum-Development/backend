8a0cc6d5a432303fcd9a2f5853ec3215e9e34ce2
divjazz03 <divjazz03@gmail.com>
2025-01-24 23:08:20 +0100

    Finally Got it working

--- app/src/main/java/genum/app/GenumApplication.java
+++ app/src/main/java/genum/app/GenumApplication.java
@@ -1,17 +1,32 @@
 package genum.app;
 
+import genum.product.model.Course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 @EnableAsync
-public class GenumApplication {
-    public static void main(String[] args) {
-       SpringApplication.run(GenumApplication.class, args);
+@EnableMongoRepositories(basePackages = {
+        "genum.product.repository",
+        "genum.dataset.repository",
+        "genum.genumUser.repository",
+        "genum.payment.repository"})
+@ComponentScan(basePackages = {"genum.product",
+        "genum.shared",
+        "genum.dataset",
+        "genum.genumUser",
+        "genum.payment"})
+@Import({genum.genumUser.config.UserWebSecurityConfiguration.class, genum.payment.config.PaymentConfiguration.class})
+public class GenumApplication
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(GenumApplication.class, args);
     }
 }
