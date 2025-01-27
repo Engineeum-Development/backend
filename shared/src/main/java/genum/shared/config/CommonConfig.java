eac9d4b1d501a72d4d8babd41767b910dd73ef56
divjazz03 <divjazz03@gmail.com>
2025-01-27 15:59:36 +0100

    added swagger dependency

--- shared/src/main/java/genum/shared/config/CommonConfig.java
+++ shared/src/main/java/genum/shared/config/CommonConfig.java
@@ -1,5 +1,9 @@
 package genum.shared.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -19,5 +23,19 @@ public class CommonConfig {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public OpenAPI recommendicOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("genum api")
+                        .description("Backend API for genum")
+                        .version("v0.0.1")
+                        .termsOfService("https://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
     }
 }
