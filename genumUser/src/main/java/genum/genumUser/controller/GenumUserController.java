f6992073b301899bf669f6ec9e64f0e00d12a93d
divjazz03 <divjazz03@gmail.com>
2025-01-27 18:58:22 +0100

    some tweaks

--- genumUser/src/main/java/genum/genumUser/controller/GenumUserController.java
+++ genumUser/src/main/java/genum/genumUser/controller/GenumUserController.java
@@ -2,9 +2,14 @@ package genum.genumUser.controller;
 
 import genum.genumUser.exception.BadRequestException;
 import genum.genumUser.exception.UserAlreadyExistsException;
+import genum.genumUser.security.exception.UserNotFoundException;
 import genum.genumUser.service.GenumUserService;
+import genum.shared.DTO.request.LoginRequest;
 import genum.shared.DTO.response.ResponseDetails;
 import genum.shared.genumUser.GenumUserDTO;
+import genum.shared.security.LoginResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -34,9 +39,25 @@ public class GenumUserController {
                     HttpStatus.CREATED.toString());
             return new ResponseEntity<>(response, HttpStatus.CREATED);
         } catch (BadRequestException e) {
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         } catch (UserAlreadyExistsException e) {
-            return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDetails<GenumUserDTO>> createUser(@Valid @RequestBody LoginRequest loginRequest,
+                                                                    HttpServletRequest servletRequest,
+                                                                    HttpServletResponse servletResponse) {
+        try{
+            LoginResponse userInfo =  userService.loginUser(loginRequest, servletRequest, servletResponse);
+            var response = new ResponseDetails<GenumUserDTO>(
+                    LocalDateTime.now(),
+                    "User was created successfully",
+                    HttpStatus.CREATED.toString());
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        } catch (UserNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- genumUser/src/main/java/genum/genumUser/service/GenumUserService.java
+++ genumUser/src/main/java/genum/genumUser/service/GenumUserService.java
@@ -5,15 +5,21 @@ import genum.genumUser.exception.BadRequestException;
 import genum.genumUser.exception.UserAlreadyExistsException;
 import genum.genumUser.model.GenumUser;
 import genum.genumUser.repository.GenumUserRepository;
+import genum.genumUser.security.exception.UserNotFoundException;
+import genum.shared.DTO.request.LoginRequest;
 import genum.shared.constant.Gender;
 import genum.shared.genumUser.GenumUserDTO;
 import genum.shared.security.CustomUserDetails;
+import genum.shared.security.LoginResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -44,6 +50,16 @@ public class GenumUserService {
             throw new BadRequestException();
         }
 
+    }
+
+    public LoginResponse loginUser(LoginRequest loginRequest, HttpServletRequest request, HttpServletResponse response) throws UserNotFoundException {
+        var userFromDatabase = genumUserRepository.findByCustomUserDetailsEmail(loginRequest.email());
+
+        if (Objects.isNull(userFromDatabase)){
+            throw new UserNotFoundException("User was not found");
+        } else{
+            return new LoginResponse(LocalDateTime.now(), "User was logged in");
+        }
     }
 
 }
