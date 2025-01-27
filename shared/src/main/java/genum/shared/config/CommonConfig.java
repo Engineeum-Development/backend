32734ffe0d17e7fecd1c63fd1321e64ed9229359
divjazz03 <divjazz03@gmail.com>
2025-01-27 16:05:55 +0100

    added swagger dependency

--- shared/src/main/java/genum/shared/config/CommonConfig.java
+++ shared/src/main/java/genum/shared/config/CommonConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.client.RestTemplate;
