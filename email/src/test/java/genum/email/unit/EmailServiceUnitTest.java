7067b4ff0fc99565e6bd0c6b658e53c3c0b54670
divjazz03 <divjazz03@gmail.com>
2025-02-15 13:09:29 +0100

    Refactored the files Added firstname and lastname to the Waiting-list request. Changed the name in the email to be the firstname

--- email/src/test/java/genum/email/unit/EmailServiceUnitTest.java
+++ email/src/test/java/genum/email/unit/EmailServiceUnitTest.java
@@ -1,6 +1,7 @@
 package genum.email.unit;
 
 
+import genum.email.constant.EmailStatus;
 import genum.email.model.Email;
 import genum.email.repository.EmailRepository;
 import genum.email.service.EmailService;
@@ -9,17 +10,23 @@ import jakarta.mail.Session;
 import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class EmailServiceUnitTest {
 
     @InjectMocks
@@ -28,10 +35,10 @@ public class EmailServiceUnitTest {
     private JavaMailSender javaMailSender;
     @Mock
     private EmailRepository emailRepository;
+    @Mock
+    private MimeMessage mimeMessage;
 
-    @Test
-    void shouldSendMail() throws MessagingException, GeneralSecurityException, IOException {
-        String message = """
+    private final String message = """
                 <html>
                     <head>
                         <title>email</title>
@@ -42,16 +49,43 @@ public class EmailServiceUnitTest {
                 </html>
                 
                 """;
-        String to = "Test Email";
-        String subject = "engineeum@gmail.com";
-        Email email = Email.builder()
-                .to(to)
-                .body(message)
-                .subject(subject)
-                .build();
-        when(emailRepository.save(email)).thenReturn(email);
-        when(javaMailSender.createMimeMessage()).thenReturn(new MimeMessage((Session) null));
-        emailService.sendEmail("Test Email" , message, "engineeum@gmail.com");
+    private final String to = "Test Email";
+    private final String subject = "engineeum@gmail.com";
+    @BeforeEach
+    void setup() {
+        emailService = new EmailService(emailRepository, javaMailSender);
+    }
+
+
+    @Test
+    void shouldSendMailSuccessfully() throws MessagingException, GeneralSecurityException, IOException {
+        when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+
+        emailService.sendEmail(subject, message, to);
+
+        verify(javaMailSender, times(1)).send(mimeMessage);
+        ArgumentCaptor<Email> emailArgumentCaptor = ArgumentCaptor.forClass(Email.class);
+        verify(emailRepository, times(1)).save(emailArgumentCaptor.capture());
+
+        Email savedEmail = emailArgumentCaptor.getValue();
+        assertEquals(EmailStatus.SUCCESS, savedEmail.getStatus());
+        assertEquals(1, savedEmail.getEmailTryAttempts());
+    }
+
+    @Test
+    void shouldHandleMailSendException() throws MessagingException {
+        when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+        doThrow(new MailSendException("SMTP failure")).when(javaMailSender).send(mimeMessage);
+
+        emailService.sendEmail(subject, message, to);
+
+        verify(javaMailSender, times(1)).send(mimeMessage);
+        ArgumentCaptor<Email> emailArgumentCaptor = ArgumentCaptor.forClass(Email.class);
+        verify(emailRepository, times(1)).save(emailArgumentCaptor.capture());
+
+        Email savedEmail = emailArgumentCaptor.getValue();
+        assertEquals(EmailStatus.FAILED, savedEmail.getStatus());
+        assertEquals(1, savedEmail.getEmailTryAttempts());
     }
 
 }
