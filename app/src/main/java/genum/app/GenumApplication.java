7fb64962c3854d98b23f3d2bfd12a370a094dbc6
divjazz03 <divjazz03@gmail.com>
2025-03-17 16:34:45 +0100

    allowed root '/' endpoint in jwt filter

--- app/src/main/java/genum/app/GenumApplication.java
+++ app/src/main/java/genum/app/GenumApplication.java
@@ -1,10 +1,16 @@
 package genum.app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.geo.GeoModule;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -32,6 +38,16 @@ public class GenumApplication
     {
 
         SpringApplication.run(GenumApplication.class, args);
+    }
+
+    @Bean
+    ObjectMapper objectMapper () {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new Jdk8Module());
+        mapper.registerModule(new GeoModule());
+        mapper.configure(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false);
+        return mapper;
     }
 
 }
