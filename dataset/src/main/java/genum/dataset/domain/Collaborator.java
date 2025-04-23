54098bac8411a94eb729249e6d21c4aa5e765abf
divjazz03 <divjazz03@gmail.com>
2025-04-23 22:43:27 +0100

    Got it ready to deploy

--- dataset/src/main/java/genum/dataset/domain/Collaborator.java
+++ dataset/src/main/java/genum/dataset/domain/Collaborator.java
@@ -1,9 +1,11 @@
 package genum.dataset.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.io.Serializable;
 
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
-public record Collaborator(String collaboratorId, CollaboratorPermission collaboratorPermission) implements Serializable {
+public record Collaborator(String collaboratorName,String collaboratorId, CollaboratorPermission collaboratorPermission) implements Serializable {
 }
