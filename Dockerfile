57ebe5de406b09fb6a55c7f07e4d9465c1c7cfb8
divjazz03 <divjazz03@gmail.com>
2025-05-04 11:52:45 +0100

    tried debugging dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM maven:3.9.9-eclipse-temurin-17-alpine as BUILDER
 
-WORKDIR /app
+WORKDIR .
 
 COPY . .
 
@@ -11,6 +11,6 @@ RUN mvn clean package -pl app -am -Dmaven.test.skip=true
 LABEL authors="divjazz, zipdemon"
 
 FROM gcr.io/distroless/java17 as FINAL
-WORKDIR /app
-COPY --from=BUILDER ./app/target/app-0.0.1-SNAPSHOT.jar ./run/
-ENTRYPOINT ["java", "-jar", "./run/app-0.0.1-SNAPSHOT.jar"]
+WORKDIR /app/genum
+COPY --from=BUILDER ./app/target/app-0.0.1-SNAPSHOT.jar .
+ENTRYPOINT ["java", "-jar", "./app-0.0.1-SNAPSHOT.jar"]
--- genumUser/src/main/java/genum/genumUser/security/RequestBodyCacheFilter.java
+++ genumUser/src/main/java/genum/genumUser/security/RequestBodyCacheFilter.java
@@ -3,17 +3,25 @@ package genum.genumUser.security;
 import genum.shared.util.CachedBodyHttpServletRequest;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
+
 @Component
 public class RequestBodyCacheFilter implements Filter {
 
+    public static final Pattern PAYMENT_PATH = Pattern.compile("/api/payment/(paystack|flutterwave)/webhook");
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        if (servletRequest instanceof HttpServletRequest){
-            CachedBodyHttpServletRequest httpServletRequest = new CachedBodyHttpServletRequest((HttpServletRequest) servletRequest);
-            filterChain.doFilter(httpServletRequest,servletResponse);
+        if (servletRequest instanceof HttpServletRequest httpServletRequest && servletResponse instanceof HttpServletResponse httpServletResponse){
+            if (PAYMENT_PATH.matcher(httpServletRequest.getRequestURI()).matches()){
+                CachedBodyHttpServletRequest cachedBodyHttpServletRequest = new CachedBodyHttpServletRequest(httpServletRequest);
+                filterChain.doFilter(cachedBodyHttpServletRequest,servletResponse);
+            } else {
+                filterChain.doFilter(httpServletRequest, httpServletResponse);
+            }
         } else {
             filterChain.doFilter(servletRequest, servletResponse);
         }
